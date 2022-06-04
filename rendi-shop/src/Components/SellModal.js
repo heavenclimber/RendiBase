3ec70b7b371f1712d17c50a4217af41b54219a78
Daledalin <jcjo92@gmail.com>
2022-06-04 20:51:59 +0700

    update report penjualan

--- rendi-shop/src/Components/SellModal.js
+++ rendi-shop/src/Components/SellModal.js
@@ -2,7 +2,11 @@ import React, { Component, useEffect, useState } from "react";
 import InputSpinner from "react-bootstrap-input-spinner";
 import { useAlert } from "react-alert";
 
+import Moment from 'react-moment';
+import 'moment-timezone';
+
 import firebase from "firebase/compat/app";
+import moment from "moment-timezone";
 
 function SellModal({ modalState, data, id }) {
   const [tempData, setTempData] = useState(data);
@@ -14,45 +18,74 @@ function SellModal({ modalState, data, id }) {
 
   const [dataSet, setDataSet] = useState();
 
-  const alert = useAlert();
+  const [datareport, setDataReport] = useState();
 
+  const alert = useAlert();
+  
 
   useEffect(() => {
     {
-      dataSet !== undefined && dataSet!==null
-        ? 
+      dataSet !== undefined && dataSet !== null &&  datareport !== undefined && datareport !== null ? (
         firebase
-            .firestore()
-            .collection("data")
-            .doc("NwE7sn9FEaKhmHGzXwmY")
-            .set({
-              item: dataSet,
-            })
-            .then((documentReference) => {
-              modalState(false);
-            })
-            .catch((error) => {
-              console.log(error.message);
-            })
-
-        : <></>;
+          .firestore()
+          .collection("data")
+          .doc("NwE7sn9FEaKhmHGzXwmY")
+          .set({
+            item: dataSet,
+          })
+          .then((documentReference) => {
+           reportSell()
+            modalState(false);
+          })
+          .catch((error) => {
+            console.log(error.message);
+          })
+      ) : (
+        <></>
+      );
     }
-  }, [dataSet]);
+  }, [dataSet],[datareport]);
 
   const setModal = () => {
     modalState(false);
   };
 
+  const reportSell = () => {
+    firebase
+      .firestore()
+      .collection("report")
+      .doc("IitGYaTQt1bf9XMWGzd2")
+      .update({
+        report: firebase.firestore.FieldValue.arrayUnion(datareport),
+      })
+      .then((documentReference) => {
+        alert.show("Sukses menjual");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
   const submitData = (e) => {
     e.preventDefault();
-    let tempqty=jmlh-buyqty
-    let dataarray=tempData
+    let tempqty = jmlh - buyqty;
+    let dataarray = tempData;
 
-    if(buyqty!==0){
-      dataarray[id].jumlah = String(tempqty)
-      setDataSet(dataarray)
+    let today = moment().format("dddd, MMMM Do YYYY, HH:mm:ss A").toString()
+
+    let buyprice = Number(dataarray[id].price) * buyqty;
+
+    if (buyqty !== 0) {
+      dataarray[id].jumlah = String(tempqty);
+      setDataReport({
+        id: id,
+        jumlah: String(buyqty),
+        name: dataarray[id].name,
+        bayar: String(buyprice),
+        date: today
+      });
     }
-
+    setDataSet(dataarray);
   };
 
   return (
--- rendi-shop/src/Views/Report.js
+++ rendi-shop/src/Views/Report.js
@@ -1,11 +1,67 @@
-import React from 'react'
-import Header from '../Components/Header'
+import React, { Component, useState, useEffect } from "react";
+import Header from "../Components/Header";
+
+import firebase from "../firebase/index";
 
 export default function Report() {
+  const [data, setData] = useState();
+
+  const [refresh, setRefresh] = useState();
+
+  console.log(data);
+
+  useEffect(() => {
+    getData();
+  }, [refresh]);
+
+  const getData = () => {
+    firebase.db
+      .collection("report")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((element) => {
+          var res = element.data();
+          setData(res.report);
+        });
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
   return (
-    <div>
-    <Header />
-    Add
-  </div>
-  )
+    <div className="main-body">
+      <Header />
+      <h2 style={{textAlign:'center', padding:"20px", paddingBottom:"10px"}}>Report Penjualan</h2>
+      {data ? (
+        <div className="reportcontainer">
+       
+          {data.map((item, i) => {
+            return <div className="reportbox">
+
+            <h5>{item.name}<span className="reportid"> #{item.id}</span></h5>
+            <div>Jumlah yang dibeli: <span className="spanreport">{item.jumlah}</span></div>
+            <div>Tanggal Pembelian: <span className="spanreport">{item.date}</span></div>
+            <div>Total yang dibayar: <span className="spanreport">{item.bayar}</span></div>
+
+            </div>;
+          })}
+        </div>
+      ) : (
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            padding: "25px",
+            textAlign: "center",
+            alignItems: "center",
+            justifyContent: "center",
+            fontWeight: "bold",
+          }}
+        >
+          There's no data
+        </div>
+      )}
+    </div>
+  );
 }
